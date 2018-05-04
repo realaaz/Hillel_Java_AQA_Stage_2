4932b3bb9d839bbbbbccf36f6a90d0398e4f6ec5
ivanzakoretskyi <ivnzak@gmail.com>
2018-05-04 20:30:27 +0300

    add AddTwoNumbersClass

---
+++ src/AddTwoNumbersClass.java
@@ -0,0 +1,16 @@
+public class AddTwoNumbersClass {
+
+    public static void main(String[] args) {
+
+        System.out.println(addNum(3, 3));
+
+
+    }
+
+    public static int addNum(int num1, int num2) {
+
+        //return num1+num2;
+        return num1-~num2-1;
+
+    }
+}
