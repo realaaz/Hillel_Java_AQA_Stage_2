52b44c61e8ba90499fdff4cafcf6b448b7fcfb46
ivanzakoretskyi <ivnzak@gmail.com>
2018-05-07 00:56:11 +0300

    L104_MoneyCostWrapClass update sum all all choco-s

--- src/L104_MoneyCostWrapClass.java
+++ src/L104_MoneyCostWrapClass.java
@@ -4,29 +4,28 @@ public class L104_MoneyCostWrapClass {
 
         ManyCostWrapClass(100, 11, 3);
 
-
     }
 
-    // money cost wrap
-    // 1 шоколадка 1 доллар
-    // можно купить 1 шоколадку за X оберок
+    // money cost wrap -> func which returns
+    // 1) 1 шоколадка X доллар
+    // 2) можно купить 1 шоколадку за Y оберок
 
     public static int ManyCostWrapClass (int money, int cost, int wrap) {
 
-        int countCh1 = 0;
-        int countCh2 = 0;
-
         // money processing
-        countCh1 = money/cost;
+        int countCh1 = money/cost;
 
         // wrappers processing
-        countCh2 = countCh1/wrap;
+        int countCh2 = countCh1/wrap;
+
+        // total chocolates processing
+        int countCh3 = countCh1 + countCh2;
 
         System.out.println("денег было при входе в магаз: " + money);
-        System.out.println("каждая шоколадка стоит: " + cost);
-        System.out.println("купили такое количество шоколадок и получили упаковки: " + countCh1);
-        System.out.println("получили шеколадки за упаковки: " + countCh2);
-
+        System.out.println("стоимость каждой шоколадки: " + cost);
+        System.out.println("купили такое количество шоколадок и получили упаковок: " + countCh1);
+        System.out.println("получили шоколадки за упаковки: " + countCh2);
+        System.out.println("всего шоколадок в итоге: " + countCh3);
 
         return  countCh2;
 
