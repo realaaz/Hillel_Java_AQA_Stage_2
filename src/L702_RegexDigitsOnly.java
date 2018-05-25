51e7c607b78a7f7a43f22a3c30de35c809e5efe2
ivanzakoretskyi <ivnzak@gmail.com>
2018-05-25 19:06:34 +0300

    update class name in L702_RegexDigitsOnl

--- src/L702_RegexDigitsOnly.java
+++ src/L702_RegexDigitsOnly.java
@@ -1,4 +1,4 @@
-public class L701_RegexDigitsOnly {
+public class L702_RegexDigitsOnly {
 
 
     public static void main(String[] args) {
@@ -24,6 +24,5 @@ public class L701_RegexDigitsOnly {
         System.out.println(outDigits);
 
     }
-
 
 }
